cf08a23292319830a8af40c80aeb68a961e061e1
MegaGastPvP <30814097+MegaGastPvP@users.noreply.github.com>
2017-11-15 12:18:33 -0500

    Update Main.php

--- src/MegaGastPvP/BPEBookCombiner/Main.php
+++ src/MegaGastPvP/BPEBookCombiner/Main.php
@@ -1,1 +1,12 @@
+<?php
+
+namespace BPEBookCombiner;
+
+use pocketmine\event\inventory\InventoryTransactionEvent;
+use pocketmine\event\Listener;
+use pocketmine\item\Item;
+use pocketmine\item\Book;
+use pocketmine\inventory\AnvilInventory;
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\plugin\PluginBase;
 
